3644ef7ce5e41cfe2e203c47804d6fd6e284564b
Marilena Istrate <marilena.istrate@gmail.com>
2016-09-12 12:46:46 +0300

    Generated WAR file

--- wiki-indexer/src/main/java/com/endava/wiki/WikiIndexerApplication.java
+++ wiki-indexer/src/main/java/com/endava/wiki/WikiIndexerApplication.java
@@ -2,9 +2,16 @@ package com.endava.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WikiIndexerApplication {
+public class WikiIndexerApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+		return application.sources(WikiIndexerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WikiIndexerApplication.class, args);
